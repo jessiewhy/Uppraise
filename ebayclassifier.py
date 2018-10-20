f04e9257ed0f7981304250d4797009b328da6658
Jessie Huang <jessiemh@ufl.edu>
2018-10-20 18:13:10 -0400

    form had to hard reload

--- ebayclassifier.py
+++ ebayclassifier.py
@@ -3,26 +3,39 @@ from ebayScraper import getListings
 from sklearn.linear_model import LinearRegression 
 from sklearn.datasets import make_regression
 import numpy as np
-from flask import Flask 
+from flask import Flask, request, render_template
 app=Flask(__name__)
 
-def getResults(item):
-    data = getListings(item)
-    
-    X, y = np.array(data[0]).reshape((-1,1)), data[1]
-    #define model
-    ebay_model= LinearRegression()
+@app.route('/py', methods = ['POST', 'GET'])
+def getResults():
+    if request.method == 'POST':
+        #item = str(request.form['item'])
+        test = ""
+        for key in request.form:
+            test=test+", "+key + "= " + request.form[key]
+        return "Item: \n" + test
+        #data = getListings(item)
+       
+        """
+        X, y = np.array(data[0]).reshape((-1,1)), data[1]
+        #define model
+        ebay_model= LinearRegression()
 
-    #fit model
-    ebay_model.fit(X,y)
-    
-    pred = [[2]]
-    #pred.reshape(1, -1)
-    return str(ebay_model.predict(pred))
+        #fit model
+        ebay_model.fit(X,y)
+        
+        pred = [[2]]
+        #pred.reshape(1, -1)
+        result = str(ebay_model.predict(pred))
 
-@app.route('/')
+        return render_template("balloons.html", result = result)
+        """
+    else:
+        return "else"
+
+@app.route('/', methods = ['GET', 'POST'])
 def result():
-    return "Result: " + getResults("iphone X")
+        return render_template("balloons.html")
     
 if __name__ == "__ebayclassifier__":
     app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,6 @@
+$(function() {
+    for (var i=0; i < 40; i++) {
+      $('.balloons').append("<div class='balloon balloon" + i + "'></div>");
+      console.log(10);
+    }
+  });
