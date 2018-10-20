a6150808d89615421fc986004d920e2c9b42349f
Your Name here <your_email@domainname.com>
2018-10-20 13:13:32 -0400

    result is now hosted on local server through flask framework

--- ebayclassifier.py
+++ ebayclassifier.py
@@ -3,19 +3,26 @@ from ebayScraper import getListings
 from sklearn.linear_model import LinearRegression 
 from sklearn.datasets import make_regression
 import numpy as np
+from flask import Flask 
+app=Flask(__name__)
 
-data = getListings("iphone X")
+def getResults(query):
+    data = getListings(query)
+    
+    X, y = np.array(data[0]).reshape((-1,1)), data[1]
+    #define model
+    ebay_model= LinearRegression()
 
+    #fit model
+    ebay_model.fit(X,y)
+    
+    pred = [[2]]
+    #pred.reshape(1, -1)
+    return str(ebay_model.predict(pred))
 
-X, y = np.array(data[0]).reshape((-1,1)), data[1]
-#define model
-ebay_model= LinearRegression()
-
-
-
-#fit model
-ebay_model.fit(X,y)
-
-pred = [[2]]
-#pred.reshape(1, -1)
-print(ebay_model.predict(pred))
+@app.route('/')
+def result():
+    return "Result: " + getResults("iphone X")
+    
+if __name__ == "__ebayclassifier__":
+    app.run(debug=True)
